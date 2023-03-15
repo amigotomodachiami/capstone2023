8a522e72a031469e6bdadc177da6ae6c1f875640
amigotomodachiami <49504671+amigotomodachiami@users.noreply.github.com>
2023-03-15 14:27:55 -0700

    add joe balance py file

---
+++ code/joe_balanced_function_20230315.py
@@ -0,0 +1,41 @@
+# Given a string with alpha-number characters and parentheses, return a string with balanced parentheses by removing the fewsest characters possible.
+# You cannont add anything to the string.
+# Balanced parentheses means that each opening parenthesis has a corresponding closing parenthesis and the pairs of parentheses are properly nested.
+
+def balance(string):
+    remove_pos = set()  # container
+    stack_pos = []  # stack
+    for index, char in enumerate(string):
+        if char not in "()":
+            continue
+        if char == "(":
+            stack_pos.append(index)
+        elif stack_pos == []:  # if ) and there is not open para
+            remove_pos.add(index)
+        else:  # there is opening, pop
+            stack_pos.pop()
+    remove_pos = remove_pos.union(set(stack_pos))  # union un-pop(left-over) open para
+    output = ""
+    for index, char in enumerate(string):
+        if index not in remove_pos:
+            output += char
+    return output
+
+string = "(we)(ff(we)"
+string1 = "()"
+string2 = "a(b)c)"
+string3 = ")("
+string4 = "(((("
+string5 = "(()()("
+string6 = ")(())("
+string7 = ")())(()()("
+string8 = "(())())"
+
+print(string1 + " -> " + balance(string1))
+print(string2 + " -> " + balance(string2))
+print(string3 + " -> " + balance(string3))
+print(string4 + " -> " + balance(string4))
+print(string5 + " -> " + balance(string5))
+print(string6 + " -> " + balance(string6))
+print(string7 + " -> " + balance(string7))
+print(string8 + " -> " + balance(string8))
